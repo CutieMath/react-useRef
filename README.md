54301359dd7d35d2697fc1781bf7de328db6b3e1
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-25 17:51:31 +1000

    useRef to store previous state value

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Learn useRef
 
 1. useRef updates state without re-render the componet
 2. useRef stores a specific reference to a DOM element
+3. useRef can store a previous state value
--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ function App() {
   const [nameState, setNameState] = useState("");
   const rederCount = useRef(1);
   const inputRef = useRef();
+  const prevName = useRef("");
 
   // 1.
   // useState causes components to re-render when the state changes.
   // useRef doens't cause re-render
   useEffect(() => {
     rederCount.current += 1;
-  });
+  }, [nameState]);
 
   // 2.
   // useRef can be used to store a reference to a DOM element.
   function focus() {
     inputRef.current.focus();
   }
+
+  // 3.
+  // useRef can be used to store the previous value of a state
+  useEffect(() => {
+    prevName.current = nameState;
+  }, [nameState]);
 
   return (
     <>
@@ -26,7 +33,9 @@ function App() {
         value={nameState}
         onChange={(e) => setNameState(e.target.value)}
       />
-      <div>My name is {nameState}</div>
+      <div>
+        My name is {nameState}, and it used to be {prevName.current}
+      </div>
       <div>I have been rendered {rederCount.current} times</div>
       <button onClick={focus}>Focus my mouse</button>
     </>
