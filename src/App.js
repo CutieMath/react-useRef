0127fbb86c59e8c56fa2b6c2ce0ebaccedbb86e3
Yuxin Ye <yuxin_ye@protonmail.com>
2022-08-25 17:41:34 +1000

    useRef doesn't cause re-render of the component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 function App() {
   const [nameState, setNameState] = useState("");
+  const rederCount = useRef(1);
+  const inputRef = useRef();
+
+  // 1.
+  // useState causes components to re-render when the state changes.
+  // useRef doens't cause re-render
+  useEffect(() => {
+    rederCount.current += 1;
+  });
+
+  // 2.
+  // useRef can be used to store a reference to a DOM element.
 
   return (
     <>
-      <input value={nameState} onChange={(e) => setNameState(e.target.value)} />
+      <input
+        ref={inputRef}
+        value={nameState}
+        onChange={(e) => setNameState(e.target.value)}
+      />
       <div>My name is {nameState}</div>
+      <div>I have been rendered {rederCount.current} times</div>
     </>
   );
 }
